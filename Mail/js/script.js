35d436aab8a0ba9fbfaedd7170caab835e785759
Ottavio8 <paolini.ottavio8@gmail.com>
2022-10-11 21:45:58 +0200

    mail terminato

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -2,3 +2,25 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+
+const userMail = prompt("Inserire la tua mail");
+
+const mail = ["ichigo@gmail.com", "aizen@gmail.com", "neliel@gmail.com", "urahara@gmail.com", "kenpachi@gmail.com", "hitsugaya@gmail.com"];
+console.log(mail);
+
+let isFound = false;
+
+for (let i = 0; i < mail.length; i++){
+    const mailFound = mail [i];
+
+    if(mailFound === userMail) {
+        isFound = true;
+    }
+
+    console.log(userMail, mailFound, isFound);
+}
+if (isFound) {
+    alert("Conosci bleach?")
+  } else {
+    alert("Mi dispiace ma non sei un fan di bleach")
+  }
