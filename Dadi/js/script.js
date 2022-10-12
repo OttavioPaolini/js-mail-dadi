d56ceb4cd733a348dfc83898b8f2cc0483491eb5
Ottavio8 <paolini.ottavio8@gmail.com>
2022-10-12 10:07:00 +0200

    terminato dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -2,5 +2,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-Math.floor(Math.random()*6) + 1;
+const userNumber = Math.floor(Math.random()*(6 - 1 + 1) ) + 1;
 
+const pcNumber = Math.floor(Math.random()*(6 - 1 + 1) ) + 1;
+
+
+console.log(userNumber, pcNumber);
+
+let message = "";
+
+if (userNumber > pcNumber){
+    message = "ha vinto il giocatore";
+}else if (userNumber < pcNumber){
+    message = "ha vinto l'IA"
+}else{
+    message = "pareggio"
+}
+alert(message)
